e82a81fb2f2312776cf4edb10f0076ce820560b0
nikita2811 <39294680+nikita2811@users.noreply.github.com>
2025-05-19 11:00:39 +0530

    trailing slash removed

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: 'code-editor-1-7n1e.onrender.com/',
+    host: 'code-editor-1-7n1e.onrender.com',
     port: 8080,
     allowedHosts: 'all', // <- this is the key line
   }
