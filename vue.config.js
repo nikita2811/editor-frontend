dd5b8a60df5f0929939e9bdfdaa9bba672cb7e0e
nikita2811 <39294680+nikita2811@users.noreply.github.com>
2025-05-19 09:40:04 +0530

    vue config updated

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: [
+      'localhost',
+      'https://code-editor-1-7n1e.onrender.com/', // Allow subdomains
+      '192.168.1.100',  // Allow LAN access
+    ],
+  }
 })
