b90ca04f9f69b20963508e676ce9fed5e7cd82c3
nikita2811 <39294680+nikita2811@users.noreply.github.com>
2025-05-19 09:56:06 +0530

    host added

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    public: 'code-editor-1-7n1e.onrender.com/',
     allowedHosts: [
       'localhost',
-      'https://code-editor-1-7n1e.onrender.com/', // Allow subdomains
+      'code-editor-1-7n1e.onrender.com/', // Allow subdomains
       '192.168.1.100',  // Allow LAN access
     ],
   }
