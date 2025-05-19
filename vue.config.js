9dd0af8673d3f438471c88080a89203e2c6caf8c
nikita2811 <39294680+nikita2811@users.noreply.github.com>
2025-05-19 11:17:07 +0530

    host updated

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: '216.24.57.4',
     port: 8080,
     allowedHosts: 'all', // <- this is the key line
   }
